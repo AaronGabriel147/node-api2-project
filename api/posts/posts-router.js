ab9a9857f804885e329450228c1ed6e6aeb943f2
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2021-11-01 16:20:20 -0700

    2 gets

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,53 @@
 // implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model');
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    }
+})
+
+
+
+// #### 2 [GET] /api/posts/:id
+
+// - If the _post_ with the specified `id` is not found:
+
+//   - return HTTP status code `404` (Not Found).
+//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
+
+// - If there's an error in retrieving the _post_ from the database:
+//   - respond with HTTP status code `500`.
+//   - return the following JSON: `{ message: "The post information could not be retrieved" }`.
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id)
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+
+
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,35 @@
 // implement your server here
 // require your posts router and connect it here
+
+
+// const express = require('express'); // import the express package
+// const server = express(); // creates the server
+// server.use(express.json());
+
+// server.use('/', (req, res) => {
+//     res.send('Hello from Express');
+// });
+
+
+// module.exports = server;
+
+
+// ________________________________________________________
+
+
+
+const express = require('express');
+const postsRouter = require('./posts/posts-router'); // import the posts router
+const server = express(); // creates the server
+
+server.use(express.json()); // 
+
+server.use('/api/posts', postsRouter); // connect the posts router to the server
+
+server.use('*', (req, res) => { // this is a middleware
+    res.status(404).json({
+        message: 'not found'
+    });
+});
+
+module.exports = server;
